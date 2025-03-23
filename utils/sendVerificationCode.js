f4dfb8d5b0fff5c6348d2f5f5d430c224c1a5b54
abdullah461 <abdullahabubakar461@.com>
2025-03-23 17:25:21 +0100

    feat: replace email sending functionality with Mailjet for verification emails

---
+++ utils/sendVerificationCode.js
@@ -0,0 +1,39 @@
+const mailjet = require('node-mailjet').apiConnect(
+    process.env.MJ_APIKEY_PUBLIC,
+    process.env.MJ_APIKEY_PRIVATE
+  )
+  const sendVerificationMail = async (email, token) => {
+    const request = mailjet.post('send', { version: 'v3.1' }).request({
+      Messages: [
+        {
+          From: {
+            Email: process.env.SENDER_EMAIL,
+            Name: 'IBBU Clearance System',
+          },
+          To: [
+            {
+              Email: email,
+              Name: 'User',
+            },
+          ],
+          Subject: 'Email Verification',
+          TextPart: `Your verification code is: ${token}`,
+          HTMLPart: `<p>Your verification code is: <strong>${token}</strong></p>`
+        },
+      ],
+    });
+  
+    try {
+      const result = await request;
+      console.log('Verification email sent:', result.body);
+    } catch (error) {
+      console.error('Error sending verification email:', error.statusCode, error.message);
+    }
+  };
+  
+  module.exports = {
+    sendVerificationMail
+  };
+  
+  
+  
