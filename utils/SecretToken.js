226669b5da130b2a97dead06712e46058bcef142
abkaaar <abdullahabubakar461@gmail.com>
2025-04-11 22:58:20 +0100

    refactor: improve createSecretToken function by adding token key validation and removing commented code

--- utils/SecretToken.js
+++ utils/SecretToken.js
@@ -1,9 +1,21 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
+// require("dotenv").config();
+// const jwt = require("jsonwebtoken");
+
+// module.exports.createSecretToken = (id) => {
+//   return jwt.sign({ id }, process.env.TOKEN_KEY, {
+//     expiresIn: 2 * 24 * 60 * 60,
+//   });
+// };
 
 module.exports.createSecretToken = (id) => {
-  return jwt.sign({ id }, process.env.TOKEN_KEY, {
-    expiresIn: 2 * 24 * 60 * 60,
+  const tokenKey = process.env.TOKEN_KEY;
+
+  if (!tokenKey) {
+    throw new Error('TOKEN_KEY is missing from environment variables');
+  }
+
+  return jwt.sign({ id }, tokenKey, {
+    expiresIn: 2 * 24 * 60 * 60,  // expires in 2 days
   });
 };
 
